3c8bfcfc29a9c8a92fb12310b62327d24ada1859
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-08-15 17:18:38 -0400

    set up header and footer

---
+++ wordpress/wp-content/themes/blogtemplate/footer.php
@@ -0,0 +1,10 @@
+</body>
+<?php if (! is_front_page()){ ?>
+<footer id="footer">
+	<div class="footer-body">
+		<p class="copywrite">© 2016 Rachel Dorn</p>
+	</div>
+</footer>
+<?php }?>
+	<?php wp_footer(); ?>
+</html>
---
+++ wordpress/wp-content/themes/blogtemplate/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta name="viewport" content="width=device-width">
+        <title><?php wp_title(); ?></title>
+        <link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('stylesheet_url'); ?>" />
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <link href='https://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class();?>>
+    	<div id="page" class="site">
+            <?php if (! is_front_page()) {?>
+    		<header class="site-header col-xs-12">
+                <div class="row header-bar">
+                    <div class="col-xs-2 logo-col">
+                        <a class="logo" href="/">R</a>
+                    </div>
+                    <div class="col-xs-9 col-xs-offset-1 menu-col">
+                        <?php wp_nav_menu( array( 'theme_location' => 'main-menu') );?>
+                    </div>
+                </div>
+            </header>
+            <?php }?>
+        </div>
